0bad94ca198032367639f59bd9a0f9e33fa2067a
Dennis <56588737+Donn-MXI@users.noreply.github.com>
2021-01-20 06:19:28 +0300

    added maps

--- app/src/main/java/com/example/admin/printqr/user_ui/Maps/MapsFragment.java
+++ app/src/main/java/com/example/admin/printqr/user_ui/Maps/MapsFragment.java
@@ -1,23 +1,110 @@
 package com.example.admin.printqr.user_ui.Maps;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.os.Looper;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 
 import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 
 import com.example.admin.printqr.R;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsFragment extends Fragment {
-
+public class MapsFragment extends Fragment implements OnMapReadyCallback {
+    private MapView mMapView;
+    private GoogleMap mMap;
+    private LocationRequest mLocationRequest;
+    private Location mLastLocation;
+    private FusedLocationProviderClient fusedLocationProviderClient;
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_maps, container, false);
 
 
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(getActivity());
+
+        mMapView = (MapView)root.findViewById(R.id.mapView);
+        mMapView.onCreate(savedInstanceState);
+        mMapView.getMapAsync(this);
+
+
         return root;
     }
+
+
+
+
+    private static final int REQUEST_CODE = 101;
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
+        mMap = googleMap;
+
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(1000);
+        mLocationRequest.setFastestInterval(1000);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+
+        if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getContext(),
+                Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_CODE);
+            return;
+
+        }
+        fusedLocationProviderClient.requestLocationUpdates(mLocationRequest,mLocationCallback, Looper.myLooper());
+        mMap.setMyLocationEnabled(true);
+    }
+
+
+
+
+
+    Marker myLocationMarker;
+    LatLng myLatLng;
+    LocationCallback mLocationCallback = new LocationCallback(){
+        @Override
+        public void onLocationResult(LocationResult locationResult) {
+
+            for (Location location : locationResult.getLocations()){
+
+                if (getContext() != null) {
+
+                    mLastLocation = location;
+                    myLatLng = new LatLng(location.getLatitude(), location.getLongitude());
+
+                    if(myLocationMarker != null){
+
+                        myLocationMarker.remove();
+                    }
+
+                    myLocationMarker = mMap.addMarker(new MarkerOptions().position(myLatLng).title("Your Location"));
+
+                    mMap.moveCamera(CameraUpdateFactory.newLatLng(myLatLng));
+                    mMap.animateCamera(CameraUpdateFactory.zoomTo(15));
+
+                }
+
+            }
+        }
+    };
 }
